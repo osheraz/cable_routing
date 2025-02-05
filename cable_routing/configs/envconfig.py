fbdd8f077ef73f3f0324aa90a7d1f501a99bb93f
Osher Azulay <osheraz@wifi-10-42-18-126.wifi.berkeley.edu>
2025-02-05 11:38:51 -0800

    update

--- cable_routing/configs/envconfig.py
+++ cable_routing/configs/envconfig.py
@@ -2,13 +2,29 @@ from typing import Literal, Optional, Tuple, Union
 import pathlib
 import tyro
 import dataclasses
+from autolab_core import RigidTransform, Point
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
+class EnvConfig:
+    board_width: float = 0.5
+    board_height: float = 0.5
+    board_thickness: float = 0.01
+    board_offset: float = 0.1
+    board_rotation: float = 0.0
+    board_translation: Tuple[float, float, float] = (0.0, 0.0, 0.0)
+    above_clip_1: RigidTransform = RigidTransform(
+        rotation=[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+        translation=[0.3, -0.2, 0.15]
+    )
+    # TODO ..
+    
+
+@dataclasses.dataclass(frozen=True)
 class YuMiConfig:
     YUMI_MIN_POS: Tuple[float, ...] = (-2.94, -2.00, -2.94, -2.16, -5.06, -1.54, -4.00)
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class CameraConfig:
     width: int = 3840
     height: int = 2160
@@ -23,8 +39,11 @@ class CameraConfig:
 class ExperimentConfig:
     camera_cfg: CameraConfig
     robot_cfg: YuMiConfig
-
+    board_cfg: EnvConfig
+    
 if __name__ == "__main__":
     args = tyro.cli(ExperimentConfig)
     dict_args = dataclasses.asdict(args)
     print(dict_args)
+    
+    
