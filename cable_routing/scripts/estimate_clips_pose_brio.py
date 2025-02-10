22b15814ec13f9f11ec4608c3fa5bd1c6af42548
osheraz <osheraz@berkeley.edu>
2025-02-10 13:47:30 -0800

    update

--- cable_routing/scripts/estimate_clips_pose_brio.py
+++ cable_routing/scripts/estimate_clips_pose_brio.py
@@ -7,14 +7,27 @@ from autolab_core import RigidTransform
 from cable_routing.env.ext_camera.utils.pcl_utils import depth_to_pointcloud
 import cv2
 
+def rescale_intrinsics(K, scale_factor):
+    K_rescaled = K.copy()
+    K_rescaled[0, 0] *= scale_factor  
+    K_rescaled[1, 1] *= scale_factor 
+    K_rescaled[0, 2] *= scale_factor 
+    K_rescaled[1, 2] *= scale_factor
+    return K_rescaled
 
-def color_segment_fixtures(point_cloud, color_threshold=(0.1, 0.1, 0.1)):
+def color_segment_fixtures(point_cloud, color_threshold=(0.1, 0.1, 0.1), display=True):
     points = np.asarray(point_cloud.points)
     colors = np.asarray(point_cloud.colors)
 
     mask = np.linalg.norm(colors - color_threshold, axis=1) < 0.2
     fixture_points = points[mask]
+    
+    if display:
+        segmented_pcd = o3d.geometry.PointCloud()
+        segmented_pcd.points = o3d.utility.Vector3dVector(fixture_points)
+        segmented_pcd.colors = o3d.utility.Vector3dVector(colors[mask])
 
+        o3d.visualization.draw_geometries([segmented_pcd])
     return fixture_points
 
 
@@ -26,7 +39,7 @@ def cluster_fixtures(fixture_points, eps=0.02, min_points=10):
         pcd.cluster_dbscan(eps=eps, min_points=min_points, print_progress=True)
     )
 
-    if labels.size == 0 or (labels == -1).all():  # Check if all points are noise
+    if labels.size == 0 or (labels == -1).all():  
         print("No valid clusters found.")
         return []
 
@@ -80,12 +93,22 @@ def visualize_results(pcd, clusters, fixture_poses):
     o3d.visualization.draw_geometries([pcd, axis] + cluster_pcds + fixture_orientations)
 
 
+
+def mask_image_outside_roi(image, roi):
+    x, y, w, h = roi
+    masked_image = image.copy()
+    masked_image[:y, :] = 255
+    masked_image[y + h:, :] = 255
+    masked_image[:, :x] = 255
+    masked_image[:, x + w:] = 255
+    return masked_image
+
 def main():
 
     script_path = Path(__file__).resolve()
     project_root = script_path.parent.parent.parent
     hdf5_file_path = (
-        project_root / "records" / "new" / "camera_data_20250209_155027_0.h5"
+        project_root / "records" / "new" / "camera_data_20250209_162231_0.h5"
     )
 
     brio_to_world_path = project_root / "data" / "brio" / "brio2world.tf"
@@ -102,20 +125,19 @@ def main():
     with h5py.File(hdf5_file_path, "r") as hdf:
         brio_rgb = hdf["brio/rgb"][0]
 
-    SCALE_FACTOR = 0.25
+    SCALE_FACTOR = 0.5
     brio_rgb = cv2.resize(
         brio_rgb, None, fx=SCALE_FACTOR, fy=SCALE_FACTOR, interpolation=cv2.INTER_AREA
     )
+    # brio_rgb = cv2.cvtColor(brio_rgb, cv2.COLOR_RGB2BGR)
+    roi = cv2.selectROI("Select Region", brio_rgb, fromCenter=False, showCrosshair=True)
+    cv2.destroyWindow("Select Region")
+    if roi == (0, 0, 0, 0):
+        roi = (0, 0, brio_rgb.shape[1], brio_rgb.shape[0])
 
+    brio_rgb = mask_image_outside_roi(brio_rgb, roi)
+    
     brio_depth = np.full(brio_rgb.shape[:2], 1.0, dtype=np.float32)
-
-    def rescale_intrinsics(K, scale_factor):
-        K_rescaled = K.copy()
-        K_rescaled[0, 0] *= scale_factor  # Scale fx
-        K_rescaled[1, 1] *= scale_factor  # Scale fy
-        K_rescaled[0, 2] *= scale_factor  # Scale cx
-        K_rescaled[1, 2] *= scale_factor  # Scale cy
-        return K_rescaled
 
     brio_intrinsics = rescale_intrinsics(brio_intrinsics, SCALE_FACTOR)
     points, colors = depth_to_pointcloud(
