03a0e4f7ca9185779935589474cc3d52989ccb4a
theodorelam1 <theodorelam1@berkeley.edu>
2025-05-28 00:52:13 +0000

    300

--- cable_routing/scripts/node_finder.py
+++ cable_routing/scripts/node_finder.py
@@ -1,8 +1,9 @@
 import cv2
+import math
 import numpy as np
 
 # Load the image
-image = cv2.imread("cable_routing/configs/board/board_setup.png")
+image = cv2.imread("cable_routing/configs/board/board.png")
 if image is None:
     raise FileNotFoundError("Image not found!")
 
@@ -10,7 +11,7 @@ if image is None:
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # Define yellow color range in HSV
-lower_yellow = np.array([30, 60,240])
+lower_yellow = np.array([20, 60,240])
 upper_yellow = np.array([35, 255, 255])
 
 # Create a binary mask where yellow is white and the rest is black
@@ -22,12 +23,22 @@ contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 # Draw contours and bounding boxes
 for cnt in contours:
     area = cv2.contourArea(cnt)
-    if area > 100:  # Filter out small blobs
+    if area < 100:  # Ignore small contours
+        continue
+    
+    perimeter = cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+    circularity = 4 * math.pi * area / (perimeter * perimeter)
+    
+    # Example condition: yellow + roughly circular + size filter
+    if circularity > 0.4 and area > 300:
         x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+
 
 # Show results
-cv2.imshow("Yellow Pegs", image)
 cv2.imshow("Mask", mask)
+cv2.imshow("Yellow Pegs", image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
